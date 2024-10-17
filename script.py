417abec728a7491a6f51fba837de28fe35185268
Ziv Yaniv <zivyaniv@nih.gov>
2024-10-17 09:56:08 -0400

    PR

--- script.py
+++ script.py
@@ -4,10 +4,10 @@ import SimpleITK as sitk
 def sitk_write_image():
     """Repeatedly call WriteImage."""
     image = sitk.Image(300, 512, 512, sitk.sitkFloat32)
-    for i in range(1000):
+    for i in range(40):
         output = f"image-{i}.nrrd"
         print(f"Saving to {output}")
-        sitk.WriteImage(image, output, True)
+        sitk.WriteImage(image, output)
 
 
 if __name__ == "__main__":
